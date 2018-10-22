313e6d3b654711797b9c3d9214d17a27ddcc8786
Emil <eschilberg@yahoo.de>
2018-10-22 14:48:56 +0200

    Emil

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 "use strict"
 
 var hinzuBtn = document.getElementById("hinzuBtn");
+var anzeigBtn= document.getElementById("anzeigBtn");
 
 function hinzufuegen() {
     var firebaseRef = firebase.database().ref();
 
     firebaseRef.child("IT For DUMMYS").set("123456789");
+}
+function anzeigen() {
+
+//    var preObject = document.getElementById('object');
+    var ulList = document.getElementById('list');
+
+    var dbRefObject = firebase.database().ref().child('buecher');
+    var dbRefList = dbRefObject.child('3258976523448');
+
+/*    dbRefObject.on('value', snap => {
+      preObject.innerText = JSON.stringify(snap.val(), null, 3);
+    });
+*/
+    dbRefList.on('child_added',  snap => {
+
+      const li = document.creatElement('li');
+      li.innerText = snap.val();
+      ulList.appendChild(li);
+    });
 }
