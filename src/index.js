887f9cb87ebe5aaf56d62fb088619a250fa987cc
Domink Kunzmann <Dominik.kunzmann@web.de>
2018-10-22 14:47:22 +0200

    Tests

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-var hinzuBtn = document.getElementById("hinzuBtn");
-
-function hinzufuegen() {
-    var firebaseRef = firebase.database().ref();
-
-    firebaseRef.child("IT For DUMMYS").set("123456789");
-}
