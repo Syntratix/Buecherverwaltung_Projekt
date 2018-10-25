934c605f7694801b6ff1d768686aedd1944d41ff
Emil <eschilberg@yahoo.de>
2018-10-25 16:03:08 +0200

    Tabelle fertig

--- src/js/index.js
+++ src/js/index.js
@@ -22,22 +22,39 @@ function hinzufuegen() {
 
     firebaseRefChild.on("child_added", snapshot => {
 
+      var childKey = snapshot.key;
       var titel = snapshot.child("titel").val();
+      var titelKey = snapshot.child("titel").key;
       var autor = snapshot.child("autor").val();
+      var autorKey = snapshot.child("autor").key;
       var jahr = snapshot.child("jahr").val();
+      var jahrKey = snapshot.child("jahr").key;
       var isbn = snapshot.child("isbn").val();
-
-      console.log(titel);
-      var a = 2;
-      console.log(a);
+      var isbnKey = snapshot.child("isbn").key;
 
       $("#tablle_body").append(
-        "<tr><td>" + titel +
-        "</td><td>" + autor +
-        "</td><td>" + jahr +
-        "</td><td>" + isbn +
+        "<tr id='"+ childKey +"'><td id='"+ titelKey + childKey +"'>" + titel +
+        "</td><td id='"+ autorKey + childKey +"'>" + autor +
+        "</td><td id='"+ jahrKey + childKey +"'>" + jahr +
+        "</td><td id='"+ isbnKey + childKey +"'>" + isbn +
         "</td></tr>"
       );
+
+
+
+
+      firebaseRefChild.on("child_changed", snapshot => {
+          var tdChanged = document.getElementById(snapshot.key);
+          location.reload(true);
+      })
+
+
+
+
+      firebaseRefChild.on("child_removed", snapshot => {
+        var tdToRemove = document.getElementById(snapshot.key);
+        tdToRemove.remove();
+      })
 
     })
 
