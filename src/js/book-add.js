113694197ea58b4f26a92ae1e52968546475b42b
Domink Kunzmann <Dominik.kunzmann@web.de>
2018-10-28 15:30:53 +0100

    Methode zum Hinzufügen der Bücher fertig. Note_module hinzugefügt für ISBN Notation (Nur Tests)

--- src/js/book-add.js
+++ src/js/book-add.js
@@ -1,9 +1,26 @@
 "use strict"
 
-var saveBtn = document.getElementById("saveBtn");
-
 function save(){
-    var firebaseRef = firebase.database().ref();
+    var bISBN = document.getElementById('bISBN').value;
+    var saveBtn = document.getElementById("saveBtn");
+    var bTitel = document.getElementById("bTitel").value;
+    var bAutor = document.getElementById("bAutor").value;
+    var bJahr = document.getElementById("bJahr").value;
+    var bAuflage = document.getElementById("bAuflage").value;
+    var bVerlag = document.getElementById("bVerlag").value;
+    var bKat = document.getElementById("katWahl").value;
+    var bText = document.getElementById("verfuegbarField").value;
 
-    
+    var firebaseRef = firebase.database().ref('buecher/');
+
+    firebaseRef.child(bISBN).set({
+        auflage: bAuflage,
+        autor: bAutor,
+        isbn: bISBN,
+        jahr: bJahr,
+        kategorie: bKat,
+        titel: bTitel,
+        verlag: bVerlag,
+        verfuegbar: bText
+    });
 }
