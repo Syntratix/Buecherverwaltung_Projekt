b1ed6b233b9f1b6312bd3e92e3fd6311f403e3f9
Domink Kunzmann <Dominik.kunzmann@web.de>
2018-10-29 15:54:24 +0100

    ISBN Check Autofill Speichern Browserify NPM Modules

--- src/js/book-add_bundle.js
+++ src/js/book-add_bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict"
 
 //Methode zum speichern des Buches
@@ -93,3 +94,58 @@ let getBookDetails = (isbn) => {
       }
   });
 }
+
+},{"is-isbn":2}],2:[function(require,module,exports){
+function isIsbn(){}
+
+isIsbn.prototype.validate = isbn => {
+	isbn = String(isbn)
+	isbn = isbn.split('')
+	if(isbn[9] === "X"){isbn[9] = "10"}
+	if(isbn.length === 10){
+		let validateSum = isbn => {
+			let sum = 0
+			isbn.forEach((digit, index) => {
+				digit = parseInt(digit)
+				sum += digit * (index+1)
+			})
+			return (sum % 11 === 0)
+		}
+		let checkSum = isbn => {
+			let check = parseInt(isbn.pop()), sum = 0
+			isbn = isbn.reverse()
+			isbn.forEach((digit, index) => {
+				sum += parseInt(digit) * (index+2)
+			})
+			sum = (11 - (sum % 11)) % 11
+			return sum === check
+		}
+		return validateSum(isbn) && validateSum(isbn.reverse()) && checkSum(isbn)
+	} else if (isbn.length === 13){
+		let checkSum = isbn => {
+			let check = parseInt(isbn.pop()), sum = 0
+			isbn.forEach((digit, index) => {
+				index++
+				if(index%2 === 0){
+					sum += parseInt(digit)*3
+				} else {
+					sum += parseInt(digit)
+				}
+			})
+			return (sum % 10) === check || (10-(sum % 10)) === check
+		}
+		return checkSum(isbn)
+	} else {
+		return false
+	}
+}
+
+module.exports = new isIsbn()
+/*
+let a = new isIsbn()
+let codes = ["030788743X","9780307887436","0670020559","9780670020553","0060523794","9780060523794"]
+codes.forEach(v=>{
+	console.log(v, a.validate(v))
+})
+*/
+},{}]},{},[1]);
