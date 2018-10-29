19ba92b6bc132bb87c4dc28cee1951193379d401
Emil <eschilberg@yahoo.de>
2018-10-29 13:38:56 +0100

    Bucherladen function.

--- src/js/index.js
+++ src/js/index.js
@@ -1,79 +1,122 @@
 
 "use strict"
 
+var firebaseRefChild = firebase.database().ref().child("buecher");
+
 function searchBooks() {
     var sucheingabe = document.getElementById('suchBar').value;
-    getBooksByTitle(sucheingabe);
+    reLoadTabel(3, sucheingabe);
 }
 
-function getBooksByTitle(titel){
-    firebase.database().ref("buecher/").once('value', function(snapshot){  /*Tabelle buecher aus Datenbank auslesen*/
-        document.getElementById('tablle_body').innerHTML = ""; /*Inhalte der Tabelle leeren*/
-        snapshot.forEach(function(book){ /*folgenden code für alle Elemente der Tabelle ausführen*/
-            if (book.val().titel.toLowerCase().includes(titel.toLowerCase())) { /*bücher welche die sucheingabe enthalten anzeigen*/
-                $("#tablle_body").append(
-                  "<tr><td><a href='html/book-detail.html'>" + book.val().titel + /*ab hier Angezeigte Tabelle mit werten beladen*/
-                  "</td><td>" + book.val().autor +
-                  "</td><td>" + book.val().jahr +
-                  "</td><td>" + book.val().isbn +
-                  "</td></tr>"
-                );
-            }
-        })
-    });
+function delTabel(){
+    document.getElementById('tablle_body').innerHTML = "";
 }
 
-function getBooksByVerfügbar(){
-    firebase.database().ref("buecher/").once('value', function(snapshot){  /*Tabelle buecher aus Datenbank auslesen*/
-        document.getElementById('tablle_body').innerHTML = ""; /*Inhalte der Tabelle leeren*/
-        snapshot.forEach(function(book){ /*folgenden code für alle Elemente der Tabelle ausführen*/
-            if (book.val().verfuegbar !== "") { /*bücher welche die sucheingabe enthalten anzeigen*/
-                $("#tablle_body").append(
-                  "<tr><td><a href='html/book-detail.html'>" + book.val().titel + /*ab hier Angezeigte Tabelle mit werten beladen*/
-                  "</td><td>" + book.val().autor +
-                  "</td><td>" + book.val().jahr +
-                  "</td><td>" + book.val().isbn +
-                  "</td></tr>"
-                );
-            }
-        })
-    });
-}
+//function getBooksByTitle(titel){
+//    firebase.database().ref("buecher/").once('value', function(snapshot){  /*Tabelle buecher aus Datenbank auslesen*/
+//        delTabel(); /*Inhalte der Tabelle leeren*/
+//        snapshot.forEach(function(book){ /*folgenden code für alle Elemente der Tabelle ausführen*/
+//            if (book.val().titel.toLowerCase().includes(titel.toLowerCase())) { /*bücher welche die sucheingabe enthalten anzeigen*/
+//                $("#tablle_body").append(
+//                  "<tr><td><a href='html/book-detail.html'>" + book.val().titel + /*ab hier Angezeigte Tabelle mit werten beladen*/
+//                  "</td><td>" + book.val().autor +
+//                  "</td><td>" + book.val().jahr +
+//                  "</td><td>" + book.val().isbn +
+//                  "</td></tr>"
+//                );
+//            }
+//        })
+//    });
+//}
+
+//function getBooksByVerfügbar(){
+//    firebase.database().ref("buecher/").once('value', function(snapshot){  /*Tabelle buecher aus Datenbank auslesen*/
+//        document.getElementById('tablle_body').innerHTML = ""; /*Inhalte der Tabelle leeren*/
+//        snapshot.forEach(function(book){ /*folgenden code für alle Elemente der Tabelle ausführen*/
+//            if (book.val().verfuegbar !== "") { /*bücher welche die sucheingabe enthalten anzeigen*/
+//                $("#tablle_body").append(
+//                  "<tr><td><a href='html/book-detail.html'>" + book.val().titel + /*ab hier Angezeigte Tabelle mit werten beladen*/
+//                  "</td><td>" + book.val().autor +
+//                  "</td><td>" + book.val().jahr +
+//                  "</td><td>" + book.val().isbn +
+//                  "</td></tr>"
+//                );
+//            }
+//        })
+//    });
+//}
 
 //tabelle
-    var firebaseRefChild = firebase.database().ref().child("buecher");
 
+function tabel(childKey, titel, autor, jahr, isbn, url){
+  $("#tablle_body").append(
+    "<tr id='"+ childKey +"'><td ><a href= "+url+">" + titel +
+    "</a></td><td>" + autor +
+    "</td><td>" + jahr +
+    "</td><td>" + isbn +
+    "</td></tr>"
+  );
+}
+
+function loadTabel(){
+    delTabel();
     firebaseRefChild.on("child_added", snapshot => {
 
       var childKey = snapshot.key;
       var titel = snapshot.child("titel").val();
-      var titelKey = snapshot.child("titel").key;
       var autor = snapshot.child("autor").val();
-      var autorKey = snapshot.child("autor").key;
       var jahr = snapshot.child("jahr").val();
-      var jahrKey = snapshot.child("jahr").key;
       var isbn = snapshot.child("isbn").val();
-      var isbnKey = snapshot.child("isbn").key;
       var url = 'html/book-detail.html?ISBN='+isbn;
 
-      $("#tablle_body").append(
-        "<tr id='"+ childKey +"'><td id='"+ titelKey + childKey +"'><a href= "+url+">" + titel +
-        "</a></td><td id='"+ autorKey + childKey +"'>" + autor +
-        "</td><td id='"+ jahrKey + childKey +"'>" + jahr +
-        "</td><td id='"+ isbnKey + childKey +"'>" + isbn +
-        "</td></tr>"
-      );
-});
+      tabel(childKey, titel, autor, jahr, isbn, url);
+  });
+}
 
-      firebaseRefChild.on("child_changed", snapshot => {
-          var tdChanged = document.getElementById(snapshot.key);
-          location.reload(true);
-      })
+function reLoadTabel(wert, searchbar){
 
+  firebaseRefChild.on("value", snapshot => {
+    delTabel();
+    var data = snapshot.val();
+    var key;
 
+    for(key in data){
+      var childKey = data[key].isbn;
+      var verf = data[key].verfuegbar;
+      var titel = data[key].titel;
+      var autor = data[key].autor;
+      var jahr = data[key].jahr;
+      var isbn = data[key].isbn;
+      var url = 'html/book-detail.html?ISBN='+isbn;
 
+      switch(wert){
+        case 1:
+        if (verf !== "") {
+          tabel(childKey, titel, autor, jahr, isbn, url);
+        }break;
+        case 2:
+         $("#tablle_body")
+          tabel(childKey, titel, autor, jahr, isbn, url);
+          break;
+        case 3:
+          if (titel.toLowerCase().includes(searchbar.toLowerCase())) {
+            if(searchbar !== ""){
+              tabel(childKey, titel, autor, jahr, isbn, url);
+            }else{
+              loadTabel();
+            }
+          }break;
+       }
+    }
+  });
+}
 
-      firebaseRefChild.on("child_removed", snapshot => {
-        var tdToRemove = document.getElementById(snapshot.key);
-        tdToRemove.remove();
-      })
+firebaseRefChild.on("child_changed", snapshot => {
+    var tdChanged = document.getElementById(snapshot.key);
+    location.reload(true);
+})
+
+firebaseRefChild.on("child_removed", snapshot => {
+  var tdToRemove = document.getElementById(snapshot.key);
+  tdToRemove.remove();
+})
